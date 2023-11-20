8c0c4d4afcf11916c1cf0d57565f010ac50ac88a
surendrakongala <surendrakongala1234@gmail.com>
2023-11-21 00:39:29 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sai(request):
+    return render(request,'sai.html')
+
+
+def sai1(request):
+    return render(request,'sai1.html')
